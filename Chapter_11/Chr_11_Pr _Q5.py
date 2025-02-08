ec097236300e83300becae61f8bbdc923b730677
manishsamota <manishsamota28@gmail.com>
2025-02-08 19:00:29 +0000

    Chr 11 Que 5 Solved

---
+++ Chapter_11/Chr_11_Pr _Q5.py
@@ -0,0 +1,33 @@
+# 5. Write a class vector representing a vector of n dimensions. Overload the + and * 
+# operator which calculates the sum and the dot(.) product of them.
+
+
+class Vector:
+    def __init__(self, x, y, z):
+        # Initialize the vector with any number of dimensions
+        self.x =x
+        self.y = y
+        self.z = z
+        
+    def __add__(self, other):
+        # Overload the + operator to add two vectors element-wise
+        result  = Vector(self.x + other.x, self.y + other.y, self.z + other.z)
+        return result
+    
+    def __mul__(self, other):
+        result =  self.x * other.x + self.y * other.y + self.z * self.z
+        return result
+    
+    def __str__(self):
+        return f"Vector ({self.x}, {self.y}, {self.z})"
+
+# Example usage:
+v1 = Vector(1, 2, 3)
+v2 = Vector(4, 5, 6)
+
+# Vector addition
+print(v1 + v2)  # Output: (5, 7, 9)
+
+# Dot product
+print(v1 * v2)  # Output: 32 (1*4 + 2*5 + 3*6)
+
