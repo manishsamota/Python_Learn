2f5a9779d6ab4af0460ab51c9cc0f303ea61c40c
manishsamota <manishsamota28@gmail.com>
2025-02-08 11:09:14 +0000

    Question 4 is added

---
+++ Chapter_10/Chr_10_Pr_Q4.py
@@ -0,0 +1,25 @@
+#2. Write a class “Calculator” capable of finding square, cube and square root of a number. 
+
+class Calculator:
+    # to take the parameters i need to define constructors
+    def __init__(self, n):
+        self.n = n
+    def square(self):
+        print(f"The square is {self.n*self.n}")
+    def cube(self):
+        print(f"The cube is  {self.n*self.n*self.n}")
+    def square_root(self):
+        print(f"The square root of number is = {self.n**1/2}")
+    @staticmethod
+    def Hello():
+        print("Hello there")
+
+obj = Calculator(4)
+
+obj.square()
+
+obj.cube()
+
+obj.square_root()
+
+obj.Hello()
