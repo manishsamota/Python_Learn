feaed0c71155d0ec023bbd475b62bc37770d253e
manishsamota <manishsamota28@gmail.com>
2025-02-09 10:37:07 +0000

    chr 11 Types Definitions in Python

--- Chapter_12/Chr_12_01_Walrus.py
+++ Chapter_12/Chr_12_01_Walrus.py
@@ -1,14 +1,26 @@
 # Walrus operator
 
-if(n:= len([1,2,3,4,5])) > 2:
-    print(f"List is too long {n}, elements, Expected <= 3")
+# if(n:= len([1,2,3,4,5])) > 2:
+#     print(f"List is too long {n}, elements, Expected <= 3")
 
-# we can write the above proram as given below 
-# it is just assigning the vaule and comparing it same time means it is doing two things at a time
+# # we can write the above proram as given below 
+# # it is just assigning the vaule and comparing it same time means it is doing two things at a time
 
-# l = [1,2,3,4,5]
-# n = len(l)
-# if(n > 3):
-#     print(f"Len of list is {n} and it is greater then given number")
-else:
-    print("Condition not match")
+# # l = [1,2,3,4,5]
+# # n = len(l)
+# # if(n > 3):
+# #     print(f"Len of list is {n} and it is greater then given number")
+# else:
+#     print("Condition not match")
+
+# TYPES DEFINITIONS IN PYTHON 
+
+age: int  = 25
+
+#function type hints
+
+def greetings(name: str) -> str:
+    return f"Hello {name} !"
+
+
+print(greetings("Manish"))
