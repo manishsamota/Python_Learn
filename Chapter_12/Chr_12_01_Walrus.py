1f35738e19b2b3ceb68f76c540eed6a1838bbbda
manishsamota <manishsamota28@gmail.com>
2025-02-09 10:29:32 +0000

    Chr 12 Walrus Oprator

---
+++ Chapter_12/Chr_12_01_Walrus.py
@@ -0,0 +1,14 @@
+# Walrus operator
+
+if(n:= len([1,2,3,4,5])) > 2:
+    print(f"List is too long {n}, elements, Expected <= 3")
+
+# we can write the above proram as given below 
+# it is just assigning the vaule and comparing it same time means it is doing two things at a time
+
+# l = [1,2,3,4,5]
+# n = len(l)
+# if(n > 3):
+#     print(f"Len of list is {n} and it is greater then given number")
+else:
+    print("Condition not match")
