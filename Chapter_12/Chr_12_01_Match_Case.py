2697b15f5d6a51977208750cbbc8b92268f2c2c1
manishsamota <manishsamota28@gmail.com>
2025-02-09 11:01:27 +0000

    chr 12 match case

--- Chapter_12/Chr_12_01_Match_Case.py
+++ Chapter_12/Chr_12_01_Match_Case.py
@@ -1,16 +1,24 @@
-def httpstatus(status):
-    match status:
-        case 200:
-            return "Ok"
-        case 404:
-            return "Not Found"
-        case 500:
-            return "Internal Server Error"
-        case _:
-            return "Unknown Error"
+# def httpstatus(status):
+#     match status:
+#         case 200:
+#             return "Ok"
+#         case 404:
+#             return "Not Found"
+#         case 500:
+#             return "Internal Server Error"
+#         case _:
+#             return "Unknown Error"
         
-print(httpstatus(200))
-print(httpstatus(404))
-print(httpstatus(500))
-print(httpstatus(305))
+# print(httpstatus(200))
+# print(httpstatus(404))
+# print(httpstatus(500))
+# print(httpstatus(305))
+
+# DICTIONARY MERGE & UPDATE OPERATORS 
+dict1  = {"a":1, "b" :2}
+dict2  = {"c":3, "d" :4}
+
+merged = dict1  | dict2
+
+print(merged)
 
